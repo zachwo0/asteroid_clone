999ec416ff4c46a911dbdf367cb842e155b92c9a
zachwo0 <zwodom0@gmail.com>
2025-05-25 19:44:03 -0400

    Edit main.py to create clock object, and delta time for FPS

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
