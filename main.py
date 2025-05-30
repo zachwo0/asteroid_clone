c050acb540d73f6095d7c1abb9839f6fb6948b95
zachwo0 <zwodom0@gmail.com>
2025-05-29 20:11:09 -0400

    Implement player left/right rotation controls with a/d keys

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1000
+        player.update(dt)
         screen.fill((0, 0, 0))
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
