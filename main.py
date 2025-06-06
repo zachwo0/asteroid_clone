ab89e8b99b0ad28473017d3f2748f4c84305a305
zachwo0 <zwodom0@gmail.com>
2025-06-05 20:30:30 -0400

    Rearranged order of variables in main.py

--- main.py
+++ main.py
@@ -19,12 +19,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        dt = clock.tick(60) / 1000
         updatable.update(dt)
         screen.fill((0, 0, 0))
         for drawables in drawable:
             drawables.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
