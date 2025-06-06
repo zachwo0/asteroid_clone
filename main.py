2549ceb0f562217e6c8446fc06939f9bd4468f8f
zachwo0 <zwodom0@gmail.com>
2025-06-06 09:29:07 -0400

    modify main.py to create sprite collection for asteroids

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
 
 
 def main():
@@ -10,7 +11,9 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
